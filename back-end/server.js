63197940c401a7867bb58c9419bf5dd111a9f2bb
marioharison51 <marioharison51@gmail.com>
2026-08-13 05:00:52 +0300

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const profilRoutes = require('./profilController');
+const authRoutes = require('./authController');
 
 app.use(express.json());
 app.use('/', profilRoutes);
+app.use('/', authRoutes);
 
 app.listen(3000, () => {
   console.log('🚀 Serveur lancé sur http://localhost:3000');
