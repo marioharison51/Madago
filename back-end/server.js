b95f97ceb3f92ef00187400c9be8914cfd5de064
marioharison51 <marioharison51@gmail.com>
2026-08-13 19:13:36 +0300

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,34 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
 const profilRoutes = require('./profilController');
 const authRoutes = require('./authController');
 const projetRoutes = require('./projetController');
+const messageRoutes = require('./messageController');
 
 app.use(express.json());
 app.use('/', profilRoutes);
 app.use('/', authRoutes);
 app.use('/', projetRoutes);
+app.use('/', messageRoutes);
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('Utilisateur connecté :', socket.id);
+
+  socket.on('envoyerMessage', (data) => {
+    io.emit('nouveauMessage', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Utilisateur déconnecté :', socket.id);
+  });
+});
+
+server.listen(3000, () => {
   console.log('🚀 Serveur lancé sur http://localhost:3000');
 });
